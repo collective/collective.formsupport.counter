109c2e27c3a3ba6b5f2ae7a0b2ab345f1f9ed532
Mauro Amico <mauro.amico@gmail.com>
2024-11-11 14:32:07 +0100

    persistentmapping

--- src/collective/formsupport/counter/events/__init__.py
+++ src/collective/formsupport/counter/events/__init__.py
@@ -1,22 +1,18 @@
 from collective.formsupport.counter.config import COUNTER_ANNOTATIONS_NAME
 from collective.formsupport.counter.config import COUNTER_ENABLED_FORM_FLAG_NAME
+from Persistence import PersistentMapping
 from zope.annotation.interfaces import IAnnotations
 
 
 def add_counter(context, event):
     """Add forms counter on the context if form requires"""
-
     if not event.form.get(COUNTER_ENABLED_FORM_FLAG_NAME):
         return
-
-    form_id = event.form_data.get("block_id")
-
+    block_id = event.form_data.get("block_id")
     annotations = IAnnotations(context)
-    counter_object = annotations.setdefault(COUNTER_ANNOTATIONS_NAME, {})
-
-    counter = counter_object.get(form_id)
-
-    if counter is None:
-        counter_object[form_id] = 0
-
-    counter_object[form_id] += 1
+    if COUNTER_ANNOTATIONS_NAME not in annotations:
+        annotations[COUNTER_ANNOTATIONS_NAME] = PersistentMapping()
+    if block_id not in annotations[COUNTER_ANNOTATIONS_NAME]:
+        annotations[COUNTER_ANNOTATIONS_NAME][block_id] = 1
+    else:
+        annotations[COUNTER_ANNOTATIONS_NAME][block_id] = +1
