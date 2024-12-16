f852bc881880ca09645e7bb69b3871726a1af61c
Roman Kysil <roman.kysil@redturtle.it>
2024-12-16 14:52:18 +0100

    Use event conditionally

--- src/collective/formsupport/counter/events/__init__.py
+++ src/collective/formsupport/counter/events/__init__.py
@@ -1,11 +1,18 @@
 from collective.formsupport.counter.config import COUNTER_ANNOTATIONS_NAME
 from collective.formsupport.counter.config import COUNTER_ENABLED_FORM_FLAG_NAME
+from collective.formsupport.counter.interfaces import ICollectiveFormsupportCounterLayer
+
 from Persistence import PersistentMapping
 from zope.annotation.interfaces import IAnnotations
+from zope.globalrequest import getRequest
 
 
 def add_counter(context, event):
     """Add forms counter on the context if form requires"""
+
+    if not ICollectiveFormsupportCounterLayer.providedBy(getRequest()):
+        return
+
     if not event.form.get(COUNTER_ENABLED_FORM_FLAG_NAME):
         return
     block_id = event.form_data.get("block_id")
